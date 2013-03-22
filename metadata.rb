5fe9000ce598aa96221751e7b4cc9b14b135689e
Kief Morris <kmorris@thoughtworks.com>
2013-03-22 18:26:51 +0000

    Removing the dynamic version number. It broke in Vagrant.

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'riemann'
 maintainer       'Nobody, really'
 maintainer_email 'none'
 description      'Installs/Configures Riemann'
-version          Time.at(`git --git-dir=#{File.join(File.dirname(__FILE__),'.git')} log --max-count=1 --pretty=format:%ct`.to_i).strftime("1.0.%Y%m%d%H%M")
+version          '1.1.0'
 
 depends          'java'
 depends          'runit'
